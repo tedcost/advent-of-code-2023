4675c9a3679fd998c93ab344010f9c04413e3d24
Ted Cost <ted.cost@crossfit.com>
2023-12-01 17:08:11 -0500

    Reorganizing work

--- day1/functions/trebuchet.py
+++ day1/functions/trebuchet.py
@@ -6,22 +6,31 @@ class Trebuchet:
     def __init__( self, coordinate_list ):
 
         self.coordinate_list = coordinate_list
-        
-    # ------- 
-    def get_coordinates( self ):
-        for coordinate in self._get_coordinates():
-            print( self.get_integers( coordinate ) )
+        self.coordinate_generator = None
 
-    def get_integers( self, coordinate ):
+    # -------
+    def get_coordinates( self ):
+        self.set_coordinates_generator()
+        self.get_integers()
+
+    def get_integers( self ):
+        for coordinate in self.coordinate_generator:
+            print ( self._get_integers( coordinate ) )
+
+    def set_coordinates_generator( self ):
+        self.coordinate_generator = self._get_coordinates_generator()
+
+
+    # -------
+    def _get_coordinates_generator( self ):
+        for coordinate in self.coordinate_list:
+            yield coordinate
+
+    def _get_integers( self, coordinate ):
         int_list = []
 
         for x in coordinate:
-            print (x)
             if x.isdigit():
                 int_list.append( x )
 
         return int_list
-
-    def _get_coordinates( self ):
-        for coordinate in self.coordinate_list:
-            yield coordinate
